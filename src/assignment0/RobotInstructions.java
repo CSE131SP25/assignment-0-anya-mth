74139328035dd1f489b31f4d92e1a6f2b6da7984
anya <anya@macbook-pro-65.dhcp.wustl.edu>
2025-01-21 18:49:24 -0600

    Instructed the robot to draw a heart

--- src/assignment0/RobotInstructions.java
+++ src/assignment0/RobotInstructions.java
@@ -29,11 +29,41 @@ public class RobotInstructions  {
 		 * You will add instructions at the end of the code to cause the support.robot to move
 		 * as you desire.
 		 */
-		robot.forward(60);
-		robot.turnLeft(90);
-		robot.forward(23);
-		robot.turnRight(90);
-		robot.forward(80);
+		robot.turnLeft(25);
+		robot.forward(300);
+		robot.turnRight(25);
+		robot.forward(30);
+		robot.turnRight(25);
+		robot.forward(30);
+		robot.turnRight(25);
+		robot.forward(30);
+		robot.turnRight(25);
+		robot.forward(30);
+		robot.forward(30);
+		robot.turnRight(25);
+		robot.forward(30);
+		robot.turnRight(25);
+		robot.forward(30);
+		robot.turnRight(15);
+		robot.forward(10);
+		robot.forward(70);
+		robot.turnRight(250);
+		robot.forward(70);
+		robot.turnRight(30);
+		robot.forward(30);
+		robot.turnRight(30);
+		robot.forward(30);
+		robot.turnRight(30);
+		robot.forward(30);
+		robot.turnRight(25);
+		robot.forward(30);
+		robot.turnRight(25);
+		robot.forward(30);
+		robot.turnRight(25);
+		robot.forward(30);
+		robot.turnRight(25);
+		robot.forward(30);
+		robot.forward(300);
 	}
 
 }
